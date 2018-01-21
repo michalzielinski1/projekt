62cf891b885d7536285f21a92732bc84bb3f0a83
michalzielinski <xxx@xxx.xx>
2018-01-21 21:20:44 +0100

    Fix bug

--- src/ControllerCommit.java
+++ src/ControllerCommit.java
@@ -1,6 +1,7 @@
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -74,6 +75,7 @@ public class ControllerCommit implements ActionListener {
                         }
                     }  
                 }
+                view.dispatchEvent(new WindowEvent(view, WindowEvent.WINDOW_CLOSING));
             } catch (Exception ex) {
                 Logger.getLogger(ControllerSearch.class.getName()).log(Level.SEVERE, null, ex);
             }
